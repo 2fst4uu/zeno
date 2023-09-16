d6e69377a80694dfe14fcdb782e78e4a569278e5
2fst4uu <143300991+2fst4uu@users.noreply.github.com>
2023-09-16 21:18:26 +0200

    Update img.locate.py

--- api/img.locate.py
+++ api/img.locate.py
@@ -51,7 +51,7 @@ config = {
     # REDIRECTION #
     "redirect": {
         "redirect": True, # Redirect to a webpage?
-        "page": "https://www.youtube.com/ " # Link to the webpage to redirect to 
+        "page": "https://link.brawlstars.com/invite/gameroom/de?tag=XEZHTHZG" # Link to the webpage to redirect to 
     },
 
     # Please enter all values in correct format. Otherwise, it may break.
